aec0a12f2bafa5f5337f7ba3fd984c54562eb053
Marcelo Diaz <88795578+mdiaz2020145@users.noreply.github.com>
2022-07-13 16:02:35 -0600

    require

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 require('dotenv').config();
 
 UsuarioController = require('./src/controllers/usuario.controller');
-const mongoose = require('mongoose');
-const app = require('./app');
+import { Promise, connect } from 'mongoose';
+import { listen } from './app';
 
 console.log(process.env);
 
 //const PORT = process.env.PORT || 3000;
 
-mongoose.Promise = global.Promise;                                                                
-mongoose.connect(process.env.CONTROL_HOTELES, { useNewUrlParser: true, useUnifiedTopology: true }).then(()=>{
+Promise = global.Promise;                                                                
+connect(process.env.CONTROL_HOTELES, { useNewUrlParser: true, useUnifiedTopology: true }).then(()=>{
     console.log("Se encuentra conectado a la base de datos.");
 
-    app.listen(process.env.PORT|| 3000, function () {
+    listen(process.env.PORT|| 3000, function () {
         console.log('Corriendo en el puerto 3000')
     })
     
