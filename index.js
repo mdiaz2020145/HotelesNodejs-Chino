e712adeaa2214bed4e7fc2d76f85a1ce90344178
Marcelo Diaz <88795578+mdiaz2020145@users.noreply.github.com>
2022-07-13 16:34:55 -0600

    aaa

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ require('dotenv').config()
 
 UsuarioController = require('./src/controllers/usuario.controller');
 
+let PORT = process.env.PORT || 3000;
+
 mongoose.Promise=global.Promise;
 mongoose.connect(process.env.HOTELES_DB_CONNECTION,{useNewUrlParser: true, useUnifiedTopology: true}).then(() => {
     console.log('Conexion a la base de datos exitosa :D')
 
-    app.listen(process.env.PORT || 3000, function () {
+    app.listen(PORT, function () {
         console.log('Corriendo en el puerto 3000 :D')
     })
 
