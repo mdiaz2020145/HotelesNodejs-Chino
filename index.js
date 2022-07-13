841f96cfff6234dfc9a57e0f3f8336322f055acf
Marcelo Diaz <88795578+mdiaz2020145@users.noreply.github.com>
2022-07-13 16:16:21 -0600

    A

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
-require('dotenv').config();
+const mongoose = require('mongoose')
+const app = require('./app')
+require('dotenv').config()
 
 UsuarioController = require('./src/controllers/usuario.controller');
-import { Promise, connect } from 'mongoose';
-import { listen } from './app';
 
-console.log(process.env);
+mongoose.Promise=global.Promise;
+mongoose.connect(process.env.HOTELES_DB_CONNECTION,{useNewUrlParser: true, useUnifiedTopology: true}).then(() => {
+    console.log('Conexion a la base de datos exitosa :D')
 
-//const PORT = process.env.PORT || 3000;
-
-Promise = global.Promise;                                                                
-connect(process.env.CONTROL_HOTELES, { useNewUrlParser: true, useUnifiedTopology: true }).then(()=>{
-    console.log("Se encuentra conectado a la base de datos.");
-
-    listen(process.env.PORT|| 3000, function () {
-        console.log('Corriendo en el puerto 3000')
+    app.listen(process.env.PORT || 3000, function () {
+        console.log('Corriendo en el puerto 3000 :D')
     })
-    
+
+    //Registrar super Admin 
     UsuarioController.RegistrarAdmin();
 
-}).catch(error => console.log(error));
+
+}).catch(err => console.log(err))
